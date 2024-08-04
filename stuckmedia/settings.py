9b2145bd02d1f31c99caa0ba0b2a8079d9fef254
CL-Ali <spyali110@gmail.com>
2024-08-04 16:20:03 +0500

    v1

--- stuckmedia/settings.py
+++ stuckmedia/settings.py
@@ -179,4 +179,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # PORT = os.getenv('PORT', 8000)
 # settings.py
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Example path where static files will be collected
+# STATIC_ROOT = BASE_DIR / 'staticfiles'  # Example path where static files will be collected
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
