eac28bbdc8b32aa46f8004b827119c312def2197
CL-Ali <spyali110@gmail.com>
2024-08-04 16:28:43 +0500

    v1

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,19 @@
 
 echo "BUILD START"
 
+# Install pip if it's not available
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3.9 get-pip.py
+    rm get-pip.py
+fi
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
