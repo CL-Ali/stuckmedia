8aabf5f1795e9b91a076b0b4a99300ecfb17ae76
CL-Ali <spyali110@gmail.com>
2024-08-04 16:03:42 +0500

    v1

--- stuckmedia/urls.py
+++ stuckmedia/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from . import views  as home
-
 
 
 
@@ -26,7 +26,7 @@ urlpatterns = [
 
     path('', home.index, name='index'),
 
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
         path('', include('api.urls')),
 
 ] 
