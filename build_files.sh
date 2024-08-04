00485844ea3f780cbbddb284a7f08c462d1948ff
CL-Ali <spyali110@gmail.com>
2024-08-04 16:38:08 +0500

    v1

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,15 @@
 
 echo "BUILD START"
 
+# Ensure pip and setuptools are installed
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+rm get-pip.py
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
