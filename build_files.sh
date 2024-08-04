bbcb009d0aee6f72fdf7aa14e71687a3ebfe3d4a
CL-Ali <spyali110@gmail.com>
2024-08-04 16:30:35 +0500

    v1

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,10 @@
 
 echo "BUILD START"
 
-# Install pip if it's not available
-if ! command -v pip &> /dev/null
-then
-    echo "pip could not be found, installing pip..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3.9 get-pip.py
-    rm get-pip.py
-fi
+# Ensure pip and setuptools are installed
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+rm get-pip.py
 
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
