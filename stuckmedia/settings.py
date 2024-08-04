afb0fc0603ab9bbf4ad197f7c9c776c8b6d61cd2
CL-Ali <spyali110@gmail.com>
2024-08-04 17:36:01 +0500

    v1

--- stuckmedia/settings.py
+++ stuckmedia/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
